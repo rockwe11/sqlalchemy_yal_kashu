11dda827afa4c1c6a2c36420238d9577e37a1522
r0ckwe11 <oleg.kashu@mail.ru>
2023-03-27 19:59:04 +0300

    Промежуточный этап создания приложения

---
+++ forms/jobs.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms import BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class JobsForm(FlaskForm):
+    job = StringField('Описание работы', validators=[DataRequired()])
+    work_size = IntegerField("Время работы", validators=[DataRequired()])
+    collaborators = StringField("Список id участников")
+    is_finished = BooleanField("Завершена")
+    submit = SubmitField('Применить')
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import datetime
 
 from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from data import db_session
 from data.jobs import Jobs
 from data.news import News
 
 from data.users import User
+from forms.jobs import JobsForm
+from forms.login import LoginForm
 from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
@@ -17,6 +22,25 @@ def index():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     return render_template("index.html", jobs=jobs)
+
+
+@app.route('/jobs',  methods=['GET', 'POST'])
+@login_required
+def add_jobs():
+    form = JobsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        jobs = Jobs()
+        jobs.job = form.job.data
+        jobs.work_size = form.work_size.data
+        jobs.collaborators = form.collaborators.data
+        jobs.is_finished = form.is_finished.data
+        current_user.jobs.append(jobs)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('jobs.html', title='Добавление работы',
+                           form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -46,6 +70,34 @@ def reqister():
         db_sess.commit()
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 def main():
